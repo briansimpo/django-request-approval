b2ae5b254b8549c63bc7768fb477b4643635dd1b
Brian Simpokolwe <briansimpokolwe@gmail.com>
2024-06-22 09:47:09 +0200

    Update __init__.py

--- src/django_request_approval/__init__.py
+++ src/django_request_approval/__init__.py
@@ -1,3 +1,3 @@
-from .models import BaseStage, BaseApprover, BaseRequest, BaseApproval
+from .models import BaseStage, BaseApprover, BaseRequest, BaseApproval, RequestStatus, ApprovalStatus
 from .services import ApproverService, ApprovalService
 from .forms import ApprovalForm
